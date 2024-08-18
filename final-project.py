785c682fc1123aa3d57e88c937f87bfa3c1bf375
Piousvir Singh <piousvir@icloud.com>
2024-08-18 13:39:26 -0500

    sorted results and fixed filter

--- final-project.py
+++ final-project.py
@@ -8,10 +8,9 @@ from dateutil import parser
 
 
 API_KEY = sensitive.API_KEY
-RESPONSE = requests.get(
-    f"https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UCYn6UEtQ771a_OWSiNBoG8w&order=date&q=sgpc%20live&eventType=completed&type=video&key={API_KEY}"
-)
+RESPONSE = requests.get(sensitive.APIREQUEST)
 data = RESPONSE.json()
+
 formatted_data = json.dumps(data, indent=2)
 with open("formatted_data.json", "w") as file:
     file.write(formatted_data)
@@ -19,7 +18,9 @@ with open("formatted_data.json", "w") as file:
 
 def main():
     filtered_data = filter_data(data)
-    write_to_csv(filtered_data)
+    print(filtered_data)
+    sorted_data = date_sort(filtered_data)
+    write_to_csv(sorted_data)
 
 
 def filter_data(data):
@@ -28,26 +29,33 @@ def filter_data(data):
     for item in data.get("items", []):
         snippet = item.get("snippet", {})
         title = snippet.get("title", "")
+        date = snippet.get("publishedAt", "")
 
         if "kirtan" in title.lower():
             filtered_data.append(
                 {
                     "title": title,
                     "url": f"https://www.youtube.com/watch?v={item['id']['videoId']}",
+                    "date" : date
                 }
             )
+    print(filtered_data)
+    return filtered_data
 
-def write_to_csv(filtered_data):
-    for _ in len(filtered_data):
-        with open("filtered_results.csv", mode="w", newline="", encoding="utf-8") as file:
-            writer = csv.DictWriter(file, fieldnames=["title", "url"])
+def write_to_csv(sorted_data):
+        with open("filtered_sorted_results.csv", mode="w", newline="", encoding="utf-8") as file:
+            writer = csv.DictWriter(file, fieldnames=["title", "url", "date"])
             writer.writeheader()
-            writer.writerows(filtered_data)
+            writer.writerows(sorted_data)
 
 
 def date_sort(filtered_data):
     for item in filtered_data:
-        date = item.get("publishedAt")
+        date = item.get("date")
         date = parser.parse(date)
-        item["publishedAt"] = date
-    filtered_data.sort(key=lambda x: x["publishedAt"], reverse=False)
+        item["date"] = date
+    filtered_data.sort(key=lambda x: x["date"], reverse=False)
+    return filtered_data
+
+if __name__ == "__main__":
+    main()
