0c9d01bc5ebe833ac7c0eb9bbf58f610414d9e02
Piousvir Singh <piousvir@icloud.com>
2024-08-18 09:29:00 -0500

    Update final-project.py

--- final-project.py
+++ final-project.py
@@ -3,24 +3,24 @@ import requests
 import csv
 import sensitive
 
-page_to_scrape = requests.get("http://quotes.toscrape.com")
-soup = BeautifulSoup(page_to_scrape.text, "html.parser")
+# page_to_scrape = requests.get("http://quotes.toscrape.com")
+# soup = BeautifulSoup(page_to_scrape.text, "html.parser")
 
-quotes = soup.find_all("span", attrs={"class":"text"})
-authors = soup.find_all("small", attrs={"class":"author"})
+# quotes = soup.find_all("span", attrs={"class":"text"})
+# authors = soup.find_all("small", attrs={"class":"author"})
 
 
-file = open("quotes.csv", "w")
-writer = csv.writer(file)
+# file = open("quotes.csv", "w")
+# writer = csv.writer(file)
 
-writer.writerow(["Quote", "Author"])
+# writer.writerow(["Quote", "Author"])
 
-for quote, author in zip(quotes, authors):
-    print(quote.text)
-    print(author.text)
-    writer.writerow([quote.text, author.text])
+# for quote, author in zip(quotes, authors):
+#     print(quote.text)
+#     print(author.text)
+#     writer.writerow([quote.text, author.text])
 
-file.close()
+# file.close()
 
 
 API_KEY = sensitive.API_KEY
