d0c709b8f667a2ba22a1042f25b30560cb44e9d9
Piousvir Singh <piousvir@icloud.com>
2024-08-18 11:55:11 -0500

    Update final-project.py

--- final-project.py
+++ final-project.py
@@ -18,30 +18,21 @@ formatted_data = json.dumps(data, indent=2)
 print(formatted_data)
 
 
-def calculating_start_time():
-    response = requests.get(
-        f"https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UCYn6UEtQ771a_OWSiNBoG8w&order=date&q=sgpc%20live&eventType=completed&type=video&key={API_KEY}"
-    )
+filtered_data = []
+print(filtered_data)
+for item in data.get("items", []):
+    snippet = item.get("snippet", {})
+    title = snippet.get("title", "")
 
+    if "kirtan" in title.lower():
+        filtered_data.append(
+            {
+                "title": title,
+                "url": f"https://www.youtube.com/watch?v={item['id']['videoId']}",
+            }
+        )
 
-def makingurl() -> str:
-    filtered_data = []
-    for item in data.get("items", []):
-        snippet = item.get("snippet", {})
-        title = snippet.get("title", "")
-
-        if "kirtan" in title.lower():
-            filtered_data.append(
-                {
-                    "title": title,
-                    "url": f"https://www.youtube.com/watch?v={item['id']['videoId']}",
-                }
-            )
-
-        return filtered_data
-
-
-with open("filtered_results.csv", mode="w", newline="", encoding="utf-8") as file:
-    writer = csv.DictWriter(file, fieldnames=["title", "url"])
-    writer.writeheader()
-    writer.writerows(filtered_data)
+    with open("filtered_results.csv", mode="w", newline="", encoding="utf-8") as file:
+        writer = csv.DictWriter(file, fieldnames=["title", "url"])
+        writer.writeheader()
+        writer.writerows(filtered_data)
