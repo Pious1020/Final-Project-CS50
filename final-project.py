cb4441258eee0e1a044077894503aee4e04e8eca
Piousvir Singh <piousvir@icloud.com>
2024-08-18 00:09:57 -0500

    testing

--- final-project.py
+++ final-project.py
@@ -1,6 +1,22 @@
-print("hello world")
-print("hello")
-print("new")
-print("testing")
-print("has")
-print("it")
+from bs4 import BeautifulSoup
+import requests
+import csv
+
+page_to_scrape = requests.get("http://quotes.toscrape.com")
+soup = BeautifulSoup(page_to_scrape.text, "html.parser")
+
+quotes = soup.find_all("span", attrs={"class":"text"})
+authors = soup.find_all("small", attrs={"class":"author"})
+
+
+file = open("quotes.csv", "w")
+writer = csv.writer(file)
+
+writer.writerow(["Quote", "Author"])
+
+for quote, author in zip(quotes, authors):
+    print(quote.text)
+    print(author.text)
+    writer.writerow([quote.text, author.text])
+
+file.close()
