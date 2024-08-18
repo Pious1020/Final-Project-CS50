e63761b78638358da4dd23397661bc0034ba6b7f
Piousvir Singh <piousvir@icloud.com>
2024-08-18 09:49:42 -0500

    response.json implement

--- final-project.py
+++ final-project.py
@@ -25,3 +25,8 @@ import sensitive
 
 API_KEY = sensitive.API_KEY
 
+response = requests.get(f"https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UCYn6UEtQ771a_OWSiNBoG8w&order=date&q=sgpc%20live&eventType=completed&type=video&key={API_KEY}")
+
+data = response.json()
+                        
+print(data)
