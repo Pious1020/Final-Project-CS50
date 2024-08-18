8977c6eaf526ac74f50b5cc90e8f2e7f54769589
Piousvir Singh <piousvir@icloud.com>
2024-08-18 10:03:03 -0500

    filtering data

--- final-project.py
+++ final-project.py
@@ -3,26 +3,6 @@ import requests
 import csv
 import sensitive
 
-# page_to_scrape = requests.get("http://quotes.toscrape.com")
-# soup = BeautifulSoup(page_to_scrape.text, "html.parser")
-
-# quotes = soup.find_all("span", attrs={"class":"text"})
-# authors = soup.find_all("small", attrs={"class":"author"})
-
-
-# file = open("quotes.csv", "w")
-# writer = csv.writer(file)
-
-# writer.writerow(["Quote", "Author"])
-
-# for quote, author in zip(quotes, authors):
-#     print(quote.text)
-#     print(author.text)
-#     writer.writerow([quote.text, author.text])
-
-# file.close()
-
-
 API_KEY = sensitive.API_KEY
 
 response = requests.get(
@@ -32,3 +12,15 @@ response = requests.get(
 data = response.json()
 
 print(data)
+
+
+filtered_data = []
+print(filtered_data)
+
+for item in data.get("items", []):
+    snippet = item.get("snippet", {})
+    title = snippet.get("title", "")
+
+print(title)
+print(filtered_data)
+print(snippet)
