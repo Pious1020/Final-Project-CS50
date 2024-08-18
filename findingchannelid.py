f6fd328052a29a03866988d29c7abd17e1771966
Piousvir Singh <piousvir@icloud.com>
2024-08-18 11:32:38 -0500

    made changes to channelid

--- findingchannelid.py
+++ findingchannelid.py
@@ -3,13 +3,19 @@ import sensitive
 import requests
 
 API_KEY = sensitive.API_KEY
-USERNAME = input("Enter the username of the channel: ")
+USERNAME = input("Enter the handle of the channel: ")
 
 url = f"https://youtube.googleapis.com/youtube/v3/channels?part=snippet&forUsername={USERNAME}&key={API_KEY}"
 
 response = requests.get(url)
 data = response.json()
-
-if 'items' in data and len(data['items']) > 0:
-    channel_id = data["items"][0]["id"]
-    print(f"channel_id: {channel_id}")
+def findingchannelid() -> str:
+    if 'items' in data and len(data['items']) > 0:
+        channel_id = data["items"][0]["id"]
+        return f"{USERNAME} = {channel_id}"
+    else:
+        print("Channel not found")
+        return None
+    
+with open("channel_id.txt", mode="w", encoding="utf-8") as file:
+    file.write(findingchannelid())
