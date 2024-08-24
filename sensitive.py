9df8f5e055fbfdc82efce83b775109bae48bc84c
Piousvir Singh <piousvir@icloud.com>
2024-08-23 23:19:47 -0500

    testing

---
+++ sensitive.py
@@ -0,0 +1,2 @@
+API_KEY = "AIzaSyCwO8bgqKS4seMcrX8hOl751GkPOfxOFlE"
+APIREQUEST = f"https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&channelId=UCYn6UEtQ771a_OWSiNBoG8w&order=date&q=sgpc%20live&eventType=completed&type=video&key={API_KEY}"
