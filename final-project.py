a82bbe18d2237a9d96a17d9db59e16a7a8317d8b
Piousvir Singh <piousvir@icloud.com>
2024-08-25 13:15:24 -0500

    cleaning code

--- final-project.py
+++ final-project.py
@@ -18,7 +18,7 @@ data = RESPONSE.json()
 formatted_data = json.dumps(data, indent=2)
 with open("formatted_data.json", "w", encoding="utf-8") as file:
     file.write(formatted_data)
-    print("Data written to JSON file successfully.")
+    # print("Data written to JSON file successfully.")
 
 # checking if time is after 4pm
 current_time = datetime.now()
@@ -27,28 +27,28 @@ now = datetime.now()
 
 
 # before and after date for api request
-afterdate = now.replace(hour=0, minute=0, second=0, microsecond=0)
+# afterdate = now.replace(hour=0, minute=0, second=0, microsecond=0)
 
-beforedate = now.replace(hour=23, minute=59, second=59, microsecond=999999)
+# beforedate = now.replace(hour=23, minute=59, second=59, microsecond=999999)
 
-afterdate = afterdate.isoformat()
-beforedate = beforedate.isoformat()
-print(afterdate)
-print(beforedate)
+# afterdate = afterdate.isoformat()
+# beforedate = beforedate.isoformat()
+# print(afterdate)
+# print(beforedate)
 
 
 def main():
     clean_data = data_cleanup(data)
-    print(clean_data)
+    # print(clean_data)
     filtered_data = filter_results_by_date(clean_data)
-    print("Filtered data: ")
+    # print("Filtered data: ")
     filtered_sorted_data = date_sort(filtered_data)
     write_to_csv(filtered_sorted_data, "filtered_sorted_data.csv")
     if current_time.time() <= set_time:
-        print("It is time to play morning kirtan.")
+        # print("It is time to play morning kirtan.")
         play_video(filtered_sorted_data[0]["url"])
     else:
-        print("It is time to play evening kirtan.")
+        # print("It is time to play evening kirtan.")
         play_video(filtered_sorted_data[2]["url"])
 
 
@@ -64,7 +64,7 @@ def data_cleanup(data: list) -> list:
             title,
         )
         if match:
-            print("Match found")
+            # print("Match found")
             filtered_data.append(
                 {
                     "title": title,
@@ -72,28 +72,26 @@ def data_cleanup(data: list) -> list:
                     "date": date,
                 }
             )
-    write_to_csv(filtered_data, "clean_data.csv")
+    # write_to_csv(filtered_data, "clean_data.csv")
     return filtered_data
 
 
 def date_sort(filtered_data: list) -> list:
     date_sorted_data = sorted(filtered_data, key=lambda x: x["date"], reverse=False)
-    write_to_csv(date_sorted_data, "date_sorted_data.csv")
+    # write_to_csv(date_sorted_data, "date_sorted_data.csv")
     return date_sorted_data
 
 
 def filter_results_by_date(sorted_data: list) -> list:
-    print("Filtering results by date.")
+    # print("Filtering results by date.")
     today = datetime.now()
     date_filtered_data = []
     for item in sorted_data:
         item["date"] = parser.parse(item.get("date", ""))
     for item in sorted_data:
-        print(f"{item["date"]}")
         if item["date"].date() == today.date():
-            print("Match found")
             date_filtered_data.append(item)
-    write_to_csv(date_filtered_data, "date_filtered_data.csv")
+    # write_to_csv(date_filtered_data, "date_filtered_data.csv")
     return date_filtered_data
 
 
@@ -103,14 +101,7 @@ def write_to_csv(json_data: list, csv_file_name: str) -> None:
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
         writer.writerows(json_data)
-        print("Data written to CSV successfully.")
-
-
-# def button_click():
-#     data = RESPONSE.json()
-#     date = data.get("items", [])[0].get("snippet", {}).get("publishedAt", "")
-#     if datetime.now().date() >= :
-#         print("Button clicked")
+        # print("Data written to CSV successfully.")
 
 
 if __name__ == "__main__":
